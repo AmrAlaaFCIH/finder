58c23f5d6b5b5de7882962472a339857770bafa2
Amr Alaa <46508070+AmrAlaaFCIH@users.noreply.github.com>
2019-08-10 17:02:51 +0200

    Github Finder App

---
+++ precache-manifest.85c01301cbb06f50dee807877af3bc7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d737bdd8ebed6bfe41e321a419a9d1f",
+    "url": "/finder/index.html"
+  },
+  {
+    "revision": "47f146242033eb332d9d",
+    "url": "/finder/static/css/main.97ea1d50.chunk.css"
+  },
+  {
+    "revision": "c27202301da8d2d19910",
+    "url": "/finder/static/js/2.23f18348.chunk.js"
+  },
+  {
+    "revision": "47f146242033eb332d9d",
+    "url": "/finder/static/js/main.1476bd25.chunk.js"
+  },
+  {
+    "revision": "812db684a4ee377028b3",
+    "url": "/finder/static/js/runtime~main.b1d1b59e.js"
+  }
+]);
